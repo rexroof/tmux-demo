d09159171ca40e21ec3a718aa558934f44f86816
Rex Roof <rexroof@gmail.com>
2018-06-26 10:27:45 -0400

    added comments

--- k8s.sh
+++ k8s.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# this is a simple script that splits your screen into 3 panes on the side
+
+# commands we want to run
 cmd_one="watch kubectl get pods"
 cmd_two="ctop"
 cmd_three="docker run -it --rm --pid=host --name htop terencewestphal/htop"
 
 
+# split full window horizonally, run cmd one
 tmux split-window -f -h $cmd_one
+
+# query pane height and pane id to calculate size of future panes
 full_height=$(tmux display -p '#{pane_height}')
 pane_id=$(tmux display -p '#{pane_id}')
 thirds=$(( $full_height / 3 ))
+
+# split new pane into a new window 1/3rd of the original size and run command
 tmux split-window -l $thirds -v -t $pane_id $cmd_three
+# (again)  split new pane into a new window 1/3rd of the original size and run command
 tmux split-window -l $thirds -v -t $pane_id $cmd_two
--- k8s2.sh
+++ k8s2.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
+# this is a more advanced version of k8s.sh
+# this will run every command in the cmds array in a separate tmux pane
+
+# the commands to run
 cmds=()
 cmds+=("watch kubectl get pods")
 cmds+=("ctop")
 cmds+=("cmatrix")
 cmds+=("docker run -it --rm --pid=host --name htop terencewestphal/htop")
 
+# count how many commands we have
 number_cmds=${#cmds[@]}
+# while there are still commands to run
 while [ -n "${cmds[0]}" ] ; do
+  # if this is our first command
   if [ -z "$first" ] ; then
-    echo tmux split-window -f -h ${cmds[0]}
+    # split window horizontally, run first command
     tmux split-window -f -h ${cmds[0]}
+
+    # figure out what our pane height should be
     full_height=$(tmux display -p '#{pane_height}')
     pane_id=$(tmux display -p '#{pane_id}')
     pane_size=$(( $full_height / $number_cmds ))
+    # mark first run as done
     first="done"
   else
+    # if not our first run, then split pane to calculated pane height and run command
     tmux split-window -l $pane_size -v -t $pane_id ${cmds[0]}
-    echo tmux split-window -l $pane_size -v -t $pane_id ${cmds[0]}
   fi
+  # pop command out of array
   cmds=("${cmds[@]:1}")
 done
-
-
